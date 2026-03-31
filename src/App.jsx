b3c698df3af387733eb536e983d78b0f0ac9eba9
Rain-bit-tree <rain@wustl.edu>
2026-03-30 20:03:03 -0500

    Removing unwanted CSS and Adding more squares.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import './App.css'
-
 export default function Square() {
-  return <button className="square">X</button>;
+  return <button className="square">X</button><button className="square">X</button>;
 }
